74aeb73c3b02c82268eabc343d59fd95cd7d0105
Mohamed ElSaadany <mhsaadany@gmail.com>
2022-03-23 03:58:52 +0300

    add grid render mode toggle switch to home page

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,35 @@
 import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
+import { Switch } from "@headlessui/react";
 import { StripeGradient } from "../lib/gradient";
 import { Gradient } from "../lib/vendor/gradient";
 
-const COLORS = ["#ef008f", "#6ec3f4", "#7038ff", "#ffba27"];
+const GRADIENT_COLORS = ["#ef008f", "#6ec3f4", "#7038ff", "#ffba27"];
 
 export default function HomePage() {
-  const [canvas_1, canvas_2] = [useRef(), useRef()];
-  const canvas_3 = useRef();
-  const [wireframeOn, setWireframeOn] = useState(true);
+  const [stripeContainer, threeContainer] = [useRef(), useRef()];
+  const [wireframe, setWireframe] = useState(false);
 
   // Setup Stripe gradienats
   useEffect(() => {
-    const gradient_1 = new Gradient().initGradient("#stripe-canvas");
-    COLORS.forEach((hex, i) => {
-      canvas_1.current.style.setProperty(`--gradient-color-${i + 1}`, hex);
-    });
-    const gradient_2 = new Gradient({ wireframe: true }).initGradient(
-      "#stripe-canvas-wireframe"
-    );
-    ["#000", "#000"].forEach((hex, i) => {
-      canvas_2.current.style.setProperty(`--gradient-color-${i + 1}`, hex);
+    const gradient = new Gradient({ wireframe }).initGradient("#stripe-canvas");
+    GRADIENT_COLORS.forEach((hex, i) => {
+      stripeContainer.current.style.setProperty(
+        `--gradient-color-${i + 1}`,
+        hex
+      );
     });
     return () => {
-      gradient_1.disconnect();
-      gradient_2.disconnect();
+      gradient.disconnect();
     };
-  }, [canvas_1, canvas_2]);
+  }, [stripeContainer, wireframe]);
 
   // Setup three.js Gradients
   useEffect(() => {
-    const gradient = new StripeGradient(canvas_3.current, {
+    const gradient = new StripeGradient(threeContainer.current, {
+      conlors: GRADIENT_COLORS,
       density: [0.06 / 2, 0.16 / 2],
-      wireframe: false,
+      wireframe,
     });
     const onResize = () => {
       gradient.resize();
@@ -45,7 +42,7 @@ export default function HomePage() {
       window.removeEventListener("resize", onResize);
       gradient.dispose();
     };
-  }, []);
+  }, [threeContainer, wireframe]);
 
   return (
     <main className="mx-auto max-w-screen-lg px-5 pb-24">
@@ -59,36 +56,51 @@ export default function HomePage() {
         </h1>
       </header>
 
-      <section className="relative mx-auto max-w-screen-lg">
+      <section className="mt-6 max-w-fit rounded-3xl border-4 border-gray-900 p-6 dark:border-white">
+        <Switch.Group className="flex select-none space-x-3" as="div">
+          <Switch.Label className="font-bold uppercase">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="h-6 w-6"
+              fill="currentColor"
+            >
+              <path d="M448 32C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H448zM152 96H64V160H152V96zM208 160H296V96H208V160zM448 96H360V160H448V96zM64 288H152V224H64V288zM296 224H208V288H296V224zM360 288H448V224H360V288zM152 352H64V416H152V352zM208 416H296V352H208V416zM448 352H360V416H448V352z" />
+            </svg>
+          </Switch.Label>
+          <Switch
+            checked={wireframe}
+            onChange={setWireframe}
+            className={`${
+              wireframe ? "bg-red-600" : "bg-black dark:bg-white"
+            } relative inline-flex h-6 w-11 items-center rounded-full`}
+          >
+            <span className="sr-only">Enable wireframe render mode</span>
+            <span
+              className={`${
+                wireframe ? "translate-x-6" : "translate-x-1"
+              } inline-block h-4 w-4 transform rounded-full bg-white dark:bg-black`}
+            />
+          </Switch>
+        </Switch.Group>
+      </section>
+
+      <section className="relative mt-6">
         <CanvasHeader>stripe&apos;s Implementation</CanvasHeader>
-        <div
-          className="relative mx-auto mt-6 h-64 max-w-screen-lg overflow-clip rounded-3xl"
-          onClick={() => {
-            setWireframeOn(!wireframeOn);
-          }}
-        >
+        <div className="mx-auto h-64 overflow-clip rounded-3xl border-4 border-black dark:border-white">
           <canvas
             id="stripe-canvas"
-            ref={canvas_1}
+            ref={stripeContainer}
             className="block h-full w-full"
-          />
-          <canvas
-            data-js-darken-top=""
-            id="stripe-canvas-wireframe"
-            ref={canvas_2}
-            className={`absolute inset-0 h-full w-full opacity-5 ${
-              wireframeOn ? "" : "hidden"
-            }`}
           />
         </div>
       </section>
 
-      <section className="relative mx-auto max-w-screen-lg">
+      <section className="relative mt-6">
         <CanvasHeader>three.js Implementation</CanvasHeader>
-        <div
-          ref={canvas_3}
-          className="relative mx-auto mt-6 h-64 max-w-screen-lg overflow-clip rounded-3xl"
-        ></div>
+        <div className="h-64 overflow-clip rounded-3xl border-4 border-black dark:border-white">
+          <canvas ref={threeContainer} />
+        </div>
       </section>
     </main>
   );
