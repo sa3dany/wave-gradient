9432e24eb3c8480cce6ef2e89ae7c639af331434
Mohamed ElSaadany <mhsaadany@gmail.com>
2022-04-06 03:21:31 +0300

    use exports only in package.json

--- packages/wave-gradients/rollup.config.js
+++ packages/wave-gradients/rollup.config.js
@@ -5,31 +5,17 @@ import pkg from "./package.json";
 const isProduction = process.env.NODE_ENV === "production";
 
 export default (async () => [
-  // browser-friendly UMD build
   {
     input: "src/main.js",
     output: {
       name: "waveGradient",
-      file: pkg.browser,
+      file: pkg.exports["."],
       format: "umd",
     },
     plugins: [
       resolve(),
       glslLoader(),
       isProduction && (await import("rollup-plugin-terser")).terser(),
-    ],
-  },
-
-  {
-    input: "src/main.js",
-    plugins: [
-      glslLoader(),
-      isProduction && (await import("rollup-plugin-terser")).terser(),
-    ],
-    external: ["three"],
-    output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
     ],
   },
 ])();
