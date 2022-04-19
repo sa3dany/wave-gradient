ffece08e9afab31f8a24bd37cfa9479db021b841
Mohamed ElSaadany <mhsaadany@gmail.com>
2022-04-20 01:31:36 +0300

    cleanup demo layout, add hover state to github link

--- demo/components/layout.js
+++ demo/components/layout.js
@@ -3,27 +3,43 @@ import { GithubIcon } from "../components/icons";
 
 export default function Layout({ children }) {
   return (
-    <div className="relative mx-auto h-full px-5">
+    <div className="mx-auto h-full px-5">
       <Head>
         <title>Wave Gradients</title>
       </Head>
 
-      {/* Navigation */}
-      <nav className="container relative z-10 mx-auto flex items-center justify-between pt-5 text-white mix-blend-overlay">
+      <nav
+        className="
+          container mx-auto
+          flex items-center justify-between pt-5
+          text-white"
+      >
         {/* Logo */}
-        <header className="select-none">
-          <h1 className="font-display text-3xl font-bold uppercase leading-none tracking-wider lg:text-4xl">
+        <header className="select-none mix-blend-overlay">
+          <h1
+            className="
+              font-display text-3xl font-bold
+              uppercase leading-none tracking-wider
+              lg:text-4xl"
+          >
             Wave Gradients
           </h1>
         </header>
+
         {/* Github repo link */}
-        <a href="https://github.com/sa3dany/wave-gradients">
+        <a
+          href="https://github.com/sa3dany/wave-gradients"
+          className="
+            mix-blend-overlay
+            hover:mix-blend-normal
+            focus:mix-blend-normal"
+        >
           <span className="sr-only">GitHub repo</span>
           <GithubIcon className="h-8 w-8 lg:h-10 lg:w-10" />
         </a>
       </nav>
 
-      {/* Page */}
+      {/* Page contents goes here */}
       {children}
     </div>
   );
