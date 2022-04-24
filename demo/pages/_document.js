6b67893155f6c7880be8a7c467dfaa59ca4b0aea
Mohamed ElSaadany <mhsaadany@gmail.com>
2022-04-24 17:22:57 +0300

    remove useless preconnect for font.googleapis.com

--- demo/pages/_document.js
+++ demo/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
