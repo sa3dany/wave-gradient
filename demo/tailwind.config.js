d5bcb8047737e96ed62fd735ddd18ab4a7d1eae0
Mohamed ElSaadany <mhsaadany@gmail.com>
2022-04-28 01:40:36 +0300

    tweak gradient placeholder fadein animation

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        fadein: "fadein 0.6s ease-in-out forwards",
+        fadein: "fadein 0.6s ease-in-out 0.6s both",
       },
       colors: {
         gray: colors.neutral,
