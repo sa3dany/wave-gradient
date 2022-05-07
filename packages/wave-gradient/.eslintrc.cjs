99265f53c297ce9a50c59f329954e09c03212479
Mohamed ElSaadany <mhsaadany@gmail.com>
2022-05-08 02:45:58 +0300

    update eslint ignore

--- packages/wave-gradient/.eslintrc.cjs
+++ packages/wave-gradient/.eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended", "plugin:jsdoc/recommended"],
+  ignorePatterns: ["dist"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
